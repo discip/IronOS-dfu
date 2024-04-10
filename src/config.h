348779e6eb8c4fd38454e21400742f0deb86ef4e
discip <53649486+discip@users.noreply.github.com>
2024-04-11 00:34:21 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -19,19 +19,21 @@
 #define FLASH_BOOTLDR_SIZE_KB         16
 #define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 112
 
-#elif MODEL_S60 || MODEL_S60P
+#if defined MODEL_S60 || MODEL_S60P
 #define GPIO_DFU_BOOT_PORT            GPIOB
 #define GPIO_DFU_BOOT_PIN             0
 #define GPIO_DP_PULLUP_PORT           GPIOA
 #define GPIO_DP_PULLUP_PIN            8
 
-#elif MODEL_S60
+#if MODEL_S60
 #define FLASH_BOOTLDR_SIZE_KB         17
 #define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 111
 
 #elif MODEL_S60P
 #define FLASH_BOOTLDR_SIZE_KB         20
 #define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 108
+#endif
+#endif
 
 #elif MODEL_TS80 || MODEL_TS80P
 #define GPIO_DFU_BOOT_PORT            GPIOB
