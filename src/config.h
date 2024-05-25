7339afa4bd6b3784e5f785f0e02d9369854e561d
discip <53649486+discip@users.noreply.github.com>
2024-05-25 23:31:25 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -11,39 +11,35 @@
 
 // Per device config
 
+#if MODEL_S60 || MODEL_S60P
+#define GPIO_DFU_BOOT_PORT            GPIOB
+#define GPIO_DFU_BOOT_PIN             0
+#define GPIO_DP_PULLUP_PORT           GPIOA
+#define GPIO_DP_PULLUP_PIN            8
+#endif
+
+#if MODEL_S60
+#define FLASH_BOOTLDR_SIZE_KB         17
+#define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 111
+
+#elif MODEL_S60P
+#define FLASH_BOOTLDR_SIZE_KB         20
+#define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 108
+#endif
+
 #ifdef MODEL_TS100
 #define GPIO_DFU_BOOT_PORT            GPIOA
 #define GPIO_DFU_BOOT_PIN             9
 #define OLED_RESET_Pin                8
 #define OLED_RESET_GPIO_Port          GPIOA
-#define FLASH_SIZE_KB                 128
 #define FLASH_BOOTLDR_SIZE_KB         16
 #define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 112
-
-#elif MODEL_S60
-#define GPIO_DFU_BOOT_PORT            GPIOB
-#define GPIO_DFU_BOOT_PIN             0
-#define GPIO_DP_PULLUP_PORT           GPIOA
-#define GPIO_DP_PULLUP_PIN            8
-#define FLASH_SIZE_KB                 128
-#define FLASH_BOOTLDR_SIZE_KB         17
-#define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 111
-
-#elif MODEL_S60P
-#define GPIO_DFU_BOOT_PORT            GPIOB
-#define GPIO_DFU_BOOT_PIN             0
-#define GPIO_DP_PULLUP_PORT           GPIOA
-#define GPIO_DP_PULLUP_PIN            8
-#define FLASH_SIZE_KB                 128
-#define FLASH_BOOTLDR_SIZE_KB         20
-#define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 108
 
 #elif MODEL_TS80 || MODEL_TS80P
 #define GPIO_DFU_BOOT_PORT            GPIOB
 #define GPIO_DFU_BOOT_PIN             1
 #define OLED_RESET_Pin                15
 #define OLED_RESET_GPIO_Port          GPIOA
-#define FLASH_SIZE_KB                 128
 #define FLASH_BOOTLDR_SIZE_KB         16
 #define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 112
 #define OLED_FLIP
@@ -53,14 +49,16 @@
 #define GPIO_DFU_BOOT_PIN             10
 #define OLED_RESET_Pin                4
 #define OLED_RESET_GPIO_Port          GPIOB
-#define FLASH_SIZE_KB                 128
 #define FLASH_BOOTLDR_SIZE_KB         32
 #define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 96
 
 #else
 #error model not defined, use model= on makefile path
 #endif
+
 // Common for all models
+
+#define FLASH_SIZE_KB 128
 #define SCL_Pin       6
 #define SCL_GPIO_Port GPIOB
 #define SDA_Pin       7
